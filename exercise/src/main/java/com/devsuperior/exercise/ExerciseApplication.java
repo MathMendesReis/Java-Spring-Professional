9051e4d9a1277123e1653057b2ec6d8911a174a0
Matheus Mendes <matheusmendesreis@outlook.com>
2025-07-05 09:28:09 -0300

    feat: execute app

--- exercise/src/main/java/com/devsuperior/exercise/ExerciseApplication.java
+++ exercise/src/main/java/com/devsuperior/exercise/ExerciseApplication.java
@@ -1,13 +1,50 @@
 package com.devsuperior.exercise;
 
+import java.text.NumberFormat;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.devsuperior.exercise.entitys.Order;
+import com.devsuperior.exercise.service.OrderService;
+import com.devsuperior.exercise.service.ShippingService;
+import java.util.Locale;
 
 @SpringBootApplication
 public class ExerciseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExerciseApplication.class, args);
+
+		Order Exemplo1 = new Order(1034, 150.00, 20.0, new OrderService(), new ShippingService());
+		Order Exemplo2 = new Order(2282, 800.00, 10.0, new OrderService(), new ShippingService());
+		Order Exemplo3 = new Order(1309, 95.00, 0.0, new OrderService(), new ShippingService());
+
+		ExerciseApplication app = new ExerciseApplication();
+
+		app.print(Exemplo1);
+		app.print(Exemplo2);
+		app.print(Exemplo3);
+
 	}
 
+	public void print(Order order) {
+		Locale localeBrasil = new Locale("pt", "BR");
+		NumberFormat numberFormat = NumberFormat.getCurrencyInstance(localeBrasil);
+		Double shipment = order.getShippingService().shipment(order);
+		Double discount = order.getOrderService().total(order);
+
+		Double calc = order.getBasic() - discount + shipment;
+
+		System.out.println("");
+		System.out.println("####");
+		System.out.println("Pedido código: " + order.getCode());
+		System.out.println("Valor total: " + numberFormat.format(calc));
+
+		// System.out.println("Desconto: " + order.getOrderService().total(order));
+		// System.out.println("Frete: " + order.getShippingService().shipment(order));
+
+		System.out.println("####");
+		System.out.println("");
+	};
 }
