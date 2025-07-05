7c0a99ed71305d3a52618c54736c0b12e42bdae7
Matheus Mendes <matheusmendesreis@outlook.com>
2025-07-05 07:30:25 -0300

    init first challeng

---
+++ desafio001/src/main/java/com/devsuperios/desafio001/Desafio001Application.java
@@ -0,0 +1,13 @@
+package com.devsuperios.desafio001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio001Application.class, args);
+	}
+
+}
---
+++ desafio001/src/test/java/com/devsuperios/desafio001/Desafio001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperios.desafio001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
